fae18fec7849a7b000582e64383a3d7c2a96b8fc
ammumike <saifiamaan445@gmail.com>
2021-03-14 21:27:01 +0530

    Batsmen details

---
+++ Lec6_LeaderBoard/activity/allMatches.js
@@ -0,0 +1,24 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const getMatch = require("./match.js");
+
+function getAllMatches(link){
+    request( link  , cb );
+}
+
+function cb(error , response , data){
+    parseData(data);
+}
+
+function parseData(html){
+    let ch = cheerio.load(html);
+    let allATags = ch('a[data-hover="Scorecard"]');
+    for(let i=0 ; i<allATags.length ; i++){
+        let aTag = allATags[i+""];
+        let link = ch(aTag).attr("href");
+        let completeLink = "https://www.espncricinfo.com"+link;
+        getMatch(completeLink);
+    }
+}
+
+module.exports = getAllMatches;
