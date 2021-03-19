6ebb9a7fc42aa36bf2637a43a61d91f255b201cf
ammumike <saifiamaan445@gmail.com>
2021-03-19 21:13:13 +0530

    github scrapper added

--- Lec7_github/activity/allRepos.js
+++ Lec7_github/activity/allRepos.js
@@ -3,26 +3,21 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const path = require("path");
 let folderPath = "./repos";
-let repoName;
+
+// let grepoName = "";
 
 function getAllRepos(link,dirName){
-    repoName = `${folderPath}/${dirName}`;
-    request(link,cb);
-}
-
-function cb(error,response,data){
-    parseData(data);
-}
-
-function parseData(html){
-    let ch = cheerio.load(html);
-    let allaTags = ch('a[data-ga-click="Explore, go to repository, location:explore feed"]');
-    fs.mkdirSync(repoName);
+    let repoName = `${folderPath}/${dirName}`;
+    request(link,function cb(error,response,data){
+        let ch = cheerio.load(data);
+        let allaTags = ch('a[data-ga-click="Explore, go to repository, location:explore feed"]');
     for(let i=0;i<15;i+=3){
         console.log(ch(allaTags[i+""]).attr("href").split("/")[2]);
         fs.mkdirSync(`${repoName}/${ch(allaTags[i+""]).attr("href").split("/")[2]}`);
     }
+    
+    });
+    fs.mkdirSync(repoName);
 }
-
 
 module.exports = getAllRepos;
