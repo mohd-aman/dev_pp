a5336884d294395536b08252bbd72d615ecc8561
ammumike <saifiamaan445@gmail.com>
2021-04-11 16:31:36 +0530

    linked_in_aut update

--- linked_in_automate/linked_in_aut.js
+++ linked_in_automate/linked_in_aut.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
-const id = "";
-const pw = "";
+const id = "saifiamaan445@gmail.com";
+const pw = "Saifiamaan445@";
 
 
 (async function(){
@@ -24,10 +24,19 @@ const pw = "";
     await tab.waitForSelector(".artdeco-pill.artdeco-pill--slate.artdeco-pill--2.artdeco-pill--choice.ember-view.search-reusables__filter-pill-button",{visible:true});
     let peopleButton = await tab.$('.artdeco-pill.artdeco-pill--slate.artdeco-pill--2.artdeco-pill--choice.ember-view.search-reusables__filter-pill-button');
     await peopleButton.click();
-    await tab.waitForSelector('.artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view',{visible:true});
-    // let connectButtons = await tab.$$('artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view');
-    // for(let i=0;i<3;i++){
-    //     await tab.click(connectButtons[i]);
-    // }
+    await tab.waitForSelector('.entity-result__title-text.t-16',{visible:true});
+    let connections = await tab.$$('.entity-result__title-text.t-16');
+    // console.log(connections[0]);
+    await tab.waitForTimeout(5000);
+    for(let i=0;i<connections.length;i++){
+        await connections[i].click();
+        await tab.waitForSelector('.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view',{visible:true});
+        await tab.waitForTimeout(5000);
+        await tab.click('.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view');
+        await tab.waitForTimeout(5000);
+        await tab.waitForSelector('.ml1.artdeco-button.artdeco-button--3.artdeco-button--primary.ember-view',{visible:true});
+        await tab.waitForTimeout(5000);
+        await tab.click('.ml1.artdeco-button.artdeco-button--3.artdeco-button--primary.ember-view');    
+    }
 
 })();
